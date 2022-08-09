1f446aa310e480cd11adfde99a35c068cfab743d
Gabriel <gabrieljch929@gmail.com>
2022-08-09 14:32:16 -0500

    Working on cart

--- db/cart.js
+++ db/cart.js
@@ -28,7 +28,40 @@ const createDataCartProducts = async ({ product_id, cart_id, quantity }) => {
   }
 };
 
+// const getAllCartData = async({ id }) {
+//   try {
+//     const { rows: routines } = await client.query(
+//       `SELECT routines.*, users.username AS "creatorName"
+//       FROM routines JOIN users ON routines."creatorId" = users.id
+//        JOIN routine_activities ON routine_activities."routineId" = routines.id
+//       WHERE routine_activities."activityId" = $1 AND routines."isPublic" = true;
+//     `,
+//       [id]
+//     );
+//     return attachActivitiesToRoutines(routines);
+//   } catch (error) {
+//     throw error;
+//   }
+
+
+const updateCartData = async ({product_id, cart_id, quantity }) => {
+  try {
+    const { rows } = await client.query(
+      `INSERT INTO cart_products(product_id, cart_id, quantity)
+    VALUES ($1)
+    FROM 
+    RETURNING *`,
+      [product_id, cart_id, quantity]
+
+    );
+    return cart;
+  } catch (error) {
+    throw error;
+  }
+};
+
 module.exports = {
   createCartData,
   createDataCartProducts,
+  updateCartData,
 };
