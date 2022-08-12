607687b3c981c83977cd9710c08c8d2475b6c471
SAMURAIM0SES <ericdiliegro@gmail.com>
2022-08-12 14:29:51 -0500

    almost done deploy

--- db/client.js
+++ db/client.js
@@ -1,6 +1,6 @@
 const { Client } = require('pg');
 
-const client = new Client('postgres://localhost:5432/restaurant_data');
+const client = new Client(process.env.DATABASE_URL ||'postgres://localhost:5432/restaurant_data');
 
 // const connectDB = async () => {
 //   const client = new Client('postgres://localhost:5432/restaurant_data');
