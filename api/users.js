e830379d309bc1fef5f2b981e3198731ce4ff588
SAMURAIM0SES <ericdiliegro@gmail.com>
2022-08-16 11:10:06 -0500

    try this?

--- api/users.js
+++ api/users.js
@@ -83,7 +83,7 @@ router.post('/login', async (req, res, next) => {
     const user = await getUserByUsername(username);
     if (user.username == username && user.password == password) {
       const token = jwt.sign(
-        { id: user.user_id, username: user.username, admin: admin},
+        { id: user.user_id, username: user.username, admin: user.admin},
         JWT_SECRET
       );
       res.send({
