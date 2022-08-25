4f36d2cf7a79173059713ffb682f3f97a9c52137
SAMURAIM0SES <ericdiliegro@gmail.com>
2022-08-25 10:45:46 -0500

    check headers

--- server.js
+++ server.js
@@ -12,9 +12,9 @@ const cors = require('cors');
 
 server.use(cors());
 server.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PATCH, DELETE, OPTIONS');
+  req.setHeader('Access-Control-Allow-Origin', '*');
+  req.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  req.setHeader('Access-Control-Allow-Methods', 'POST, GET, PATCH, DELETE, OPTIONS');
   next();
 });
 server.use(
