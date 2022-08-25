6289bd62bde5d7a5decc2e86ee68ba3647a73734
SAMURAIM0SES <ericdiliegro@gmail.com>
2022-08-25 10:50:48 -0500

    tring netlify

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ server.use(function (req, res, next) {
 });
 server.use(
   cors({
-      origin: "http://localhost:3000", 
+      origin: "https://630791cfe8949617875ca688--brilliant-kleicha-8a4278.netlify.app/", 
       credentials: true,
   })
 );
